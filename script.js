b62239b16dc7a26bf2b6f70faeb66720003c8f4e
Eunice <52475753+eunicejihyun@users.noreply.github.com>
2022-11-20 21:56:59 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+today = new Date();
+
+let copyright_year = document.getElementById('year');
+
+copyright_year.textContent=today.getFullYear();
+
+let dayOfWeek = today.getDay();
+
+todaysHours = document.getElementById('dow'+dayOfWeek)
+
+todaysHours.style.fontWeight = 500;
