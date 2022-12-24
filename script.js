6156c4b5f36b4c9b9d3a14600cdcc701956ee010
eunicejihyun <eunicejihyun@gmail.com>
2022-12-24 18:36:00 -0500

    holiday hours update

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ todaysHours = document.getElementById('dow' + dayOfWeek)
 todaysHours.style.fontWeight = 500;
 
 // SHOW MESSAGE
-let modalTitle = 'Shop Hours Update';
-let modalMessage = 'We will be closed on <strong>Thursday</strong> (11/24) and <strong>Friday</strong> (11/25) for the Thanksgiving holiday. We will reopen on Saturday (11/26). <br>Happy Thanksgiving to you and yours!';
-let modalImage = 'https://images.unsplash.com/photo-1510194638421-92f54ce46444?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2135&q=80';
-const startDate = new Date('November 20, 2022')
-const endDate = new Date('November 26, 2022')
+let modalTitle = 'Holiday Hours Update';
+let modalMessage = 'We will be closed on <strong>Monday</strong> (December 26) and <strong>Monday</strong> (January 2). Happy holidays to you and yours!';
+let modalImage = 'https://images.unsplash.com/photo-1546074177-31bfa593f731?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80';
+const startDate = new Date('December 24, 2022')
+const endDate = new Date('January 3, 2023')
 
 $(document).ready(function () {
     if (today >= startDate && today <= endDate) {
